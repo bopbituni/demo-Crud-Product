5255287715ce932d4f08259f61f4eddad2a46a05
bopbituni <thanhngau199@gmail.com>
2019-11-07 13:50:50 +0700

    delete

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -12,14 +12,4 @@
     </form>
 
 @endsection
-=======
-<form>
-Name: <input name="name"  type="text" placeholder="input your name">
-    <br>
-    Email <input type="text" placeholder="input your email" name="email">
-    <br>
-    Password <input type="text" placeholder="password" name="password">
-    <button type="submit" class="btn btn-primary">Update</button>
-    <button class="btn btn-secondary" onclick="window.history.go(-1); return false;">Hủy</button>
-</form>
->>>>>>> c1323468bd4e509c39be2377ce98134962c15852
+
