cbb82e1ab165d062cc9f3a2a7f07f702331bd230
bopbituni <thanhngau199@gmail.com>
2019-11-07 11:36:16 +0700

    Edit create form

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,8 +1,13 @@
-<form>
-Name: <input name="name"  type="text" placeholder="input your name">
-    <br>
-    Email <input type="text" placeholder="input your email" name="email">
-    <br>
-    Password <input type="text" placeholder="password" name="password">
-    <input type="submit" value="Add new">
-</form>
+@extends('layouts.master')
+@section('content')
+    <form action="{{route('user.store')}}" method="post">
+        @csrf
+        Name: <input name="name"  type="text" placeholder="input your name">
+        <br>
+        Email <input type="text" placeholder="input your email" name="email">
+        <br>
+        Password <input type="text" placeholder="password" name="password">
+        <input type="submit" value="Add new">
+    </form>
+
+@endsection
