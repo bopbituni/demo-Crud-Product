ab78060134e7cd1028d288778be58b9224b2728d
bopbituni <thanhngau199@gmail.com>
2019-11-07 14:30:57 +0700

    Fix list user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,7 +32,8 @@ class UserController extends Controller
      */
     public function index()
     {
-
+        $users = User::all();
+        return view('user.index' , compact('users'));
     }
 
     /**
