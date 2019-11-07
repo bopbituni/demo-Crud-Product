ddd4bbb08bb8e48e2642dbb2da0eaf7e687176a5
vanvinh1234 <vanvinhbk90hoangkim@gmail.com>
2019-11-07 14:53:40 +0700

    change

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-<form>
-Name: <input name="name"  type="text" placeholder="input your name">
-    <br>
-    Email <input type="text" placeholder="input your email" name="email">
-    <br>
-    Password <input type="text" placeholder="password" name="password">
-    <input type="submit" value="Add new">
-</form>
-=======
+
 @extends('layouts.master')
 @section('content')
     <form>
@@ -20,4 +11,4 @@ Name: <input name="name"  type="text" placeholder="input your name">
         <button class="btn btn-secondary" onclick="window.history.go(-1); return false;">Hủy</button>
     </form>
 @stop
->>>>>>> 7dcdf4497808fd27d41193f840339d4dc989252f
+
