6868b2e05deaad7534270501c32709e3a91e6501
vanvinh1234 <vanvinhbk90hoangkim@gmail.com>
2019-11-07 14:39:21 +0700

    hii

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -4,6 +4,5 @@ Name: <input name="name"  type="text" placeholder="input your name">
     Email <input type="text" placeholder="input your email" name="email">
     <br>
     Password <input type="text" placeholder="password" name="password">
-    <button type="submit" class="btn btn-primary">Update</button>
-    <button class="btn btn-secondary" onclick="window.history.go(-1); return false;">Hủy</button>
+    <input type="submit" value="Add new">
 </form>
