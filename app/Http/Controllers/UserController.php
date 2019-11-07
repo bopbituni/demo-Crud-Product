1c7524a25eae640e8007423b40ffa67fd0cfacc7
vanvinh1234 <vanvinhbk90hoangkim@gmail.com>
2019-11-07 11:23:10 +0700

    create controller edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,23 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function destroy($id) {
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('user.update', compact('user'));
+    }
+    public function update(Request $request, $id){
+        $user = User::findOrFail($id);
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = $request->input('password');
+        $user->save();
+        return redirect()->route('user.index');
+    }
+
+
+    public function destroy($id)
+    {
         $user = User::findOrFail($id);
         $user->delete();
         return redirect()->route('user.index');
